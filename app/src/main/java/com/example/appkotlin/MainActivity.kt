a6ae9abf9b56c03bac49427c4432a091e61f9872
Perennial2002 <pendragon05082002@gmail.com>
2022-01-28 20:16:58 +0500

    updated

--- app/src/main/java/com/example/appkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin/MainActivity.kt
@@ -4,18 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.KeyEvent
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textInputLayout: TextInputLayout
+    private lateinit var textInputPassword: TextInputLayout
     private lateinit var textInputEditText: TextInputEditText
+    private lateinit var confirmButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,39 +22,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        textInputLayout = findViewById(R.id.layout_input)
+        textInputPassword = findViewById(R.id.ttl_password)
         textInputEditText = findViewById(R.id.edt_input)
+        confirmButton = findViewById(R.id.btn_confirm)
 
-        textChangedListener()
-
-        onKeyListener()
-    }
-
-
-    private fun onKeyListener() {
-        textInputEditText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                Toast.makeText(applicationContext, "goooooo", Toast.LENGTH_SHORT).show()
-                return@OnKeyListener true
+        confirmButton.setOnClickListener {
+            if (textInputEditText.text.toString().isBlank()) {
+                textInputPassword.error = "Empty field"
             }
-            false
-        })
+        }
 
-    }
-
-    private fun textChangedListener() {
         textInputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                if (s.toString()[s.toString().length - 1].isDigit()) {
-                    textInputLayout.error = "Digit input"
-                } else {
-                    textInputLayout.isErrorEnabled = false
+                if (s.toString().length > 12) {
+                    textInputPassword.error = "Exceeded the length"
+                }else{
+                    textInputPassword.isErrorEnabled = false
                 }
             }
+
         })
     }
 }
